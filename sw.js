19060715afdd03bc884c2b8927916f939d501677
Helveticone <office@helveticonemedia.ch>
2026-01-29 19:00:12 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'cafebar31-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
